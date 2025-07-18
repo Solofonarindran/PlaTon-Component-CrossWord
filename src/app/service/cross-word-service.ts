a3c90ed15fe14b8f3e94f8ed6ca546011ce07190
Solofonarindran <solofonarindraherinantenaina@gmail.com>
2025-07-18 21:34:58 +0000

    feat(service) : service to generate a grid by library

--- src/app/service/cross-word-service.ts
+++ src/app/service/cross-word-service.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@angular/core';
-import {generateLayout} from 'crossword-layout-generator';
 import { Word } from '../model/word';
+// ❌ Pas d'import ES6 ici car on utilise "export ="
+// import CrosswordLayout from 'crossword-layout-generator';
+
+import * as clg from 'crossword-layout-generator';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrossWordService {
- generateGrid(words : Word []) {
-  const layout = generateLayout(
-    words.map(word => ({ 
-      clue: word.clue, 
-      answer: word.answer.toUpperCase()
-    }))
-  );
-  return layout;
- }
+
+  generateGrid(words: { clue : string; answer: string }[]) {
+    const layout = clg.generateLayout(words);
+    return layout
+  }
+  
 }
+
+
+
