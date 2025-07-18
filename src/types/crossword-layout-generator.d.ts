99f1d41241a66d3022920ae9ae198f32b078f0c6
Solofonarindran <solofonarindraherinantenaina@gmail.com>
2025-07-18 21:37:58 +0000

    build(crossword-layout) : declare a module crossword to build the library

--- src/types/crossword-layout-generator.d.ts
+++ src/types/crossword-layout-generator.d.ts
@@ -1,14 +1,2 @@
-declare module 'crossword-layout-generator' {
-    export function generateLayout(words: { clue: string; answer: string; }[]): {
-      size: number;
-      words: Array<
-        {
-          clue: string;
-          answer: string;
-          x: number;
-          y: number;
-          direction: 'across' | 'down';
-        }
-      >;
-    };
-}
+
+declare module 'crossword-layout-generator'
