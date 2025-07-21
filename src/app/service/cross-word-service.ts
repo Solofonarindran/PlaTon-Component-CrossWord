9fe75efd0e64acc34d30f235b6a924dead14a79d
Solofonarindran <solofonarindraherinantenaina@gmail.com>
2025-07-21 16:27:07 +0000

    refactor(crossWordService) : refactoring code , recieved the method from crossword component

--- src/app/service/cross-word-service.ts
+++ src/app/service/cross-word-service.ts
@@ -4,17 +4,50 @@ import { Word } from '../model/word';
 // import CrosswordLayout from 'crossword-layout-generator';
 
 import * as clg from 'crossword-layout-generator';
+import { Result } from '../model/result';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrossWordService {
-
-  generateGrid(words: { clue : string; answer: string }[]) {
-    const layout = clg.generateLayout(words);
-    return layout
-  }
+  private grid: string[][] = [];
+  private results: Result[] = [];
   
+  generateGridService(words: { clue : string; answer: string }[]) {
+    const layout = clg.generateLayout(words);
+    this.grid = layout.table;
+    this.results = layout.result;
+  }
+
+  /* getter of retrieve grid */
+  getGrid(): string[][] {
+    const grid = this.grid;
+    return grid;
+  }
+
+  /* getter of retrieve result */
+  getResults(): Result[] {
+    const result = this.results
+    return result;
+  }
+
+  /* check if the cell is the start of a word */
+  private isStartCell(x: number, y: number, result: Result): boolean{
+    return result.startx === x + 1 && result.starty === y + 1
+  }
+
+  /* check if the cell is a vertical word */
+  isStartCellDownService(x: number, y: number): boolean {
+    return this.results.some(result => result.orientation === "down" 
+                                       && this.isStartCell(x,y,result))
+  }
+
+  /* check if the cell is a horizental word */
+  isStartCellAcrossService(x: number, y: number): boolean {
+    return this.results.some(result => result.orientation === "across" 
+                                       && this.isStartCell(x,y,result))
+  }
+
 }
 
 
